7e0740f2059fed21e09c05b0af5886fb58abc559
lc <1641767724@qq.com>
2025-06-07 23:07:27 +0800

    客户端调用远程服务

--- client/main.go
+++ client/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	pb "GRPC/server/proto"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+func main() {
+	// 连接server
+	connect, err := grpc.Dial("127.0.0.1:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect %v", err)
+	}
+	defer connect.Close()
+
+	// 建立连接
+	client := pb.NewSayHelloClient(connect)
+
+	// 执行rpc调用
+	say, _ := client.Say(context.Background(), &pb.SayRequest{Name: "LC"})
+	fmt.Println(say)
+}
